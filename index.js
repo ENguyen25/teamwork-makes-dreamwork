cfb41d28e96b0c5bd3e8e86453a5d7975ff53103
Evelyn Nguyen <enguyen6208@gmail.com>
2021-09-07 12:51:16 -0700

    finished creating html base page with cards

--- index.js
+++ index.js
@@ -42,41 +42,91 @@ inquirer
     //   }
   ])
   .then((answers) => {
-    //   const htmlFile = `<!DOCTYPE html>
-    //   <html lang="en">
-    //   <head>
-    //       <meta charset="UTF-8">
-    //       <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    //       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //       <title>Team Profile Generator</title>
-    //   </head>
-    //   <body>
-          
-    //   </body>
-    //   </html>`
-
-    //   fs.writeFile('index.html', content, (err) => err ? console.log(err) : console.log('Success!'));
-
-    console.log(testArray)
+    const contentCardsArray = []
 
     const teamList = testArray.loopyLoop
 
-    console.log(teamList)
-
-    for (var i = 0; i < teamList.length; i++) {
-        if (teamList[i].teamOptions === 'Manager') {
-            const splitInfo = teamList[i].manager.split(',')
-            const newEmployee = new Manager(splitInfo[0], splitInfo[1], splitInfo[2], splitInfo[3])
-            console.log(newEmployee)
-        }
-    }
-
-    const newCard = () => {
+    const newManagerCard = (manager) => {
         const content = `<div class="content">
             <div class="card-header">
-                <h1 class="name">
+                <h1 class="name">${manager.name}</h1>
             </div>
-            <div class="card-info"></div>
+            <div class="card-info">
+                <p>ID: ${manager.id}</p>
+                <p>Email: ${manager.email}</p>
+                <p>Office Number: ${manager.officeNumber}</p>
+            </div>
         </div>`
+        contentCardsArray.push(content)
     }
+
+    const newEngineerCard = (engineer) => {
+          const content = `        <div class="content">
+            <div class="card-header">
+                <h1 class="name">${engineer.name}</h1>
+            </div>
+            <div class="card-info">
+                <p>ID: ${engineer.id}</p>
+                <p>Email: ${engineer.email}</p>
+                <p>GitHub: ${engineer.github}</p>
+            </div>
+          </div>`
+        contentCardsArray.push(content)
+    }
+
+    const newInternCard = (intern) => {
+        const content = `        <div class="content">
+            <div class="card-header">
+                <h1 class="name">${intern.name}</h1>
+                </div>
+            <div class="card-info">
+                <p>ID: ${intern.id}</p>
+                <p>Email: ${intern.email}</p>
+                <p>School: ${intern.school}</p>
+            </div>
+        </div>`
+        contentCardsArray.push(content)
+
+    }
+
+    for (var i = 0; i < teamList.length; i++) {
+      if (teamList[i].teamOptions === 'Manager') {
+          const splitInfo = teamList[i].manager.split(',').map(item => item.trim())
+          const newEmployee = new Manager(splitInfo[0], splitInfo[1], splitInfo[2], splitInfo[3])
+          newManagerCard(newEmployee)
+      } else if (teamList[i].teamOptions === 'Engineer') {
+          const splitInfo = teamList[i].engineer.split(',').map(item => item.trim())
+          const newEmployee = new Engineer(splitInfo[0], splitInfo[1], splitInfo[2], splitInfo[3])
+          newEngineerCard(newEmployee)
+      } else {
+          const splitInfo = teamList[i].intern.split(',').map(item => item.trim())
+          const newEmployee = new Intern(splitInfo[0], splitInfo[1], splitInfo[2], splitInfo[3])
+          newInternCard(newEmployee)
+      }
+    }
+
+    const contentCards = contentCardsArray.join('\n')
+
+    const htmlFile = `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Awesome Team!</title>
+</head>
+<body>
+
+<header>My Awesome Team!</header>
+
+    <div class="container">
+
+        ${contentCards}
+
+    </div>
+      
+</body>
+</html>`
+
+      fs.writeFile('index.html', htmlFile, (err) => err ? console.log(err) : console.log('Success!'));
   })
